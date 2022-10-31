7ec3e2b406fae55807b9a40a1ad340172867c243
Nikita Madorsky <madorskii@mail.ru>
2022-10-31 17:56:12 +0300

    m00-ex03 done

--- ex03/Program.java
+++ ex03/Program.java
@@ -1,39 +1,57 @@
 import java.util.Scanner;
 
 public class Program {
+    private static long storage = 0L;
+
+    private static boolean checkUserInput(int i, String line) {
+        if (line.equals("42")) {
+            return true;
+        }
+        if (line.equals("Week " + i) == false) {
+            System.err.println("theIllegalArgument");
+            System.exit(-1);
+        }
+        return false;
+    }
+
+    private static int getMin(Scanner in) {
+        int grade;
+        int minGrade = 9;
+        for (int i = 0; i < 5; i++) {
+            grade = in.nextInt();
+            if (grade < minGrade) {
+                minGrade = grade;
+            }
+        }
+        return minGrade;
+    }
+
+    private static void displayOneWeek(int grade) {
+        for (int i = 0; i < grade; i++) {
+            System.out.print("=");
+        }
+        System.out.println(">");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        int sum, countDigit, result = 0;
-        while (true) {
-            if (num == 42) {
+        int weekDay = 0, rank = 1;
+        while (weekDay <= 18) {
+            if (checkUserInput(++weekDay, in.nextLine())) {
                 break;
             }
-            if (num < 0) {
-                num = in.nextInt();
-                continue;
-            }
-            countDigit = 0;
-            for (int div = 1; num / div > 0; div *= 10) {
-                countDigit++;
-            }
-            sum = 0;
-            for (int i = 0, mod = 10, div = 1; i < countDigit; mod *= 10, div *= 10, i++) {
-                sum += num % mod / div;
-            }
-            boolean isPrime = true;
-            for (int j = 2; j * j <= sum; j++) {
-                if (sum % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                result += 1;
-            }
-            num = in.nextInt();
+            storage += getMin(in) * rank;
+            rank *= 10;
+            in.nextLine();
         }
         in.close();
-        System.err.println("Count of coffee - request - " + result);
+        int mod = 10, div = 1;
+        for (int i = 0; i < weekDay - 1; i++) {
+            int res = (int) (storage % mod / div);
+            System.out.print("Week " + (i + 1) + " ");
+            displayOneWeek(res);
+            mod *= 10;
+            div *= 10;
+        }
     }
 }
