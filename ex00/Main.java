9f5a5e28d856be51063c7ad6b616fc81969c8f33
Nikita Madorsky <madorskii@mail.ru>
2022-10-31 10:22:46 +0300

    m00-ex00 fixed. removed args

--- ex00/Main.java
+++ ex00/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String regex = "[0-9]+";
-        if (args.length != 1 || args[0].length() != 6 || args[0].matches(regex) == false) {
+        int num = 479598;
+        String numAsString = Integer.toString(num);
+        if (numAsString.length() != 6) {
            System.out.println("Error");
         } else {
             int result = 0;
-            String arg = args[0];
             for (int i = 0; i < 6; i++) {
-               result += Character.getNumericValue(arg.charAt(i));
+               result += Character.getNumericValue(numAsString.charAt(i));
             }
            System.out.println(result);
         }
