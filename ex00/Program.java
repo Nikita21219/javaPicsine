afb300559f56a3b546a5dba41bee748659827e2c
Nikita Madorsky <madorskii@mail.ru>
2022-10-31 10:44:21 +0300

    remove for loop.......

--- ex00/Program.java
+++ ex00/Program.java
@@ -2,9 +2,12 @@ public class Program {
     public static void main(String[] args) {
         int number = 479598;
         int result = 0;
-        for (int i = 0, mod = 10, div = 1; i < 6; i++, mod *= 10, div *= 10) {
-            result += number % mod / div;
-        }
+        result += number % 10;
+        result += number % 100 / 10;
+        result += number % 1000 / 100;
+        result += number % 10000 / 1000;
+        result += number % 100000 / 10000;
+        result += number % 1000000 / 100000;
         System.out.println(result);
     }
 }
